8b28ea951b08c1f3379edd01805a454e19687ba0
Bozca <abdullahbozca@gmail.com>
2022-05-09 23:07:49 +0300

    9-05-2022

---
+++ src/test/java/pages/MainPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class MainPage {
+
+    public MainPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy (xpath = "//img[@class='img-fluid radius10 radius24']")
+    public WebElement firstImg;
+}
---
+++ src/test/resources/BrokenImages.feature
@@ -0,0 +1,7 @@
+
+Feature: Validate if there are broken images
+
+
+  Scenario: validate if there are broken images
+  Given images control
+
